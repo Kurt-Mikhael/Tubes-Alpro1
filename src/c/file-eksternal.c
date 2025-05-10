5e7ad66a7d26f165335fe0b99a0af959f66a900f
Zannn210 <fauzan210mohamad@gmail.com>
2025-05-10 08:51:24 +0700

    Update file-eksternal.c

--- src/c/file-eksternal.c
+++ src/c/file-eksternal.c
@@ -75,6 +75,62 @@ void bacaPenyakitCSV(ListPenyakit* lp, const char* filename) {
     fclose(file);
 }
 
+void bacaObatPenyakit(ListPenyakit daftar_penyakit, MapObatPenyakit* map){
+    FILE* file = fopen("obat_penyakit.csv", "r");
+    if(!file){
+        printf("file tidak dapat dibuka");
+        return NULL;
+
+    }
+    int count = 0;
+    char buffer[64];
+    fgets(buffer, sizeof(buffer), file);
+
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        (count)++;
+    }
+    rewind (file);
+    obatpenyakit* data =  (obatpenyakit*)malloc((count) * sizeof(obatpenyakit));
+
+    fgets(buffer, sizeof(buffer), file);
+
+    int idx = 0;
+    while (fgets(buffer, sizeof(buffer), file))
+    {
+        int nilai = 0;
+        int cols = 0;
+        int i = 0;
+        while (buffer[i] != '\0')
+        {
+            if(buffer[i] >= '0' && buffer[i] <= '9'){
+                nilai = nilai * 10 + (buffer[i] - '0');
+                i++;
+            }
+            else if(buffer[i] == ',' || buffer[i] == '\n'){
+                
+                if(cols == 0){
+                    data[idx].obat_id = nilai;
+                }
+                else if(cols == 1){
+                    data[idx].penyakit_id = nilai;
+                }
+                else if(cols == 2){
+                    data[idx].urutan_minum = nilai;
+                }
+                i++;
+                nilai = 0;
+                cols++;
+            }
+        }
+        idx++;
+
+    }
+    // data obat untuk penyakit di simpan di obatpenyakit* data
+
+    fclose(file);
+    
+}
 /*
 Membaca file config.txt lalu memindahkan datanya ke variabel-variabel yang sesuai
 */
